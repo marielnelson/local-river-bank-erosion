ce2c08631069ad6e3650abe4669f0508278d11d0
marielnelson <marielnelson@utexas.edu>
2025-10-12 20:59:52 -0500

    create function to load bank profiles

--- src/load_data.py
+++ src/load_data.py
@@ -1,16 +1,28 @@
 import glob
 import os
 import pandas as pd
+from src.bank_profile import BankProfile
 
-def load_transects(file_pattern):
-    csv_files = glob.glob(file_pattern)
-    
+# think about whether these should be separate or a single function that returns both the df and the profiles
+
+def create_bank_profiles_df(file_pattern):
+
+    files = glob.glob(file_pattern)
     dfs = []
-    for file in csv_files:
+    for file in files:
         basename = os.path.basename(file)
         date_str = basename.split('join_')[1].split('-transect')[0]
         df = pd.read_csv(file)
         df['survey_date'] = date_str
         dfs.append(df)
-    
+
     return pd.concat(dfs, ignore_index=True)
+
+def create_bank_profiles(profile_df):
+    
+    profiles = []
+    for (idx, date), group in profile_df.groupby(['transect_idx', 'survey_date']):
+        t = BankProfile(group, idx, date)
+        profiles.append(t)
+    
+    return profiles
