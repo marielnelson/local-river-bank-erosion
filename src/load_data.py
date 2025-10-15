deff5e5e8a2ed35964638d90348b03c8d9e123a5
marielnelson <marielnelson@utexas.edu>
2025-10-15 12:32:39 -0500

    get imports working

--- src/load_data.py
+++ src/load_data.py
@@ -5,8 +5,7 @@ from src.bank_profile import BankProfile
 
 # think about whether these should be separate or a single function that returns both the df and the profiles
 
-def create_bank_profiles_df(file_pattern):
-
+def create_bank_profile_df(file_pattern):
     files = glob.glob(file_pattern)
     dfs = []
     for file in files:
@@ -15,14 +14,11 @@ def create_bank_profiles_df(file_pattern):
         df = pd.read_csv(file)
         df['survey_date'] = date_str
         dfs.append(df)
-
     return pd.concat(dfs, ignore_index=True)
 
-def create_bank_profiles(profile_df):
-    
+def convert_to_bank_profile_class(profile_df):
     profiles = []
     for (idx, date), group in profile_df.groupby(['transect_idx', 'survey_date']):
         t = BankProfile(group, idx, date)
         profiles.append(t)
-    
     return profiles
